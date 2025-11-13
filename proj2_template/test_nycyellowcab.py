54ba9df4a88a8a4ff05bbaa4c0625293c7e85f6b
wlong1 <109259315+wlong1@users.noreply.github.com>
2025-11-13 12:01:38 -0500

    Added filter

--- proj2_template/test_nycyellowcab.py
+++ proj2_template/test_nycyellowcab.py
@@ -11,10 +11,18 @@ import haversine_library
 
 #taxi = cudf.read_parquet("yellow_tripdata_2009-01.parquet")
 df = pd.DataFrame()
+start_lon = -74.15
+start_lat = 40.5774
+end_lon = -73.7004
+end_lat = 40.9176
 for i in range(12):
     filename = f"/tmp/tlcdata/yellow_tripdata_2009-{i+1:02d}.parquet"
     cur = pd.read_parquet(filename)
+    cur = cur[(cur['Start_Lon'] > start_lon) & (cur['Start_Lon'] > start_lat) & (cur['End_Lon'] > end_lat) & (cur['End_Lat'] > end_lat)]
+
     df = pd.concat([df, cur])
+
+
 
 
 x1=df['Start_Lon'].to_numpy()
