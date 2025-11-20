8a576f61748d045012d8750474d75320d445fbad
tat400pinglife <dzhang765@gmail.com>
2025-11-20 11:47:43 -0500

    pure python and filter adjustment

--- proj2_template/test_purepython.py
+++ proj2_template/test_purepython.py
@@ -1,26 +1,42 @@
 import sys
 import numpy as np
-sys.path.append('./build')
 
 import pandas as pd
 #import cudf
-import haversine_library
+import timeit
 
 #code from: https://github.com/rapidsai/cuspatial/blob/724d170a2105441a3533b5eaf9ee82ddcfc49be0/notebooks/nyc_taxi_years_correlation.ipynb
 #data from https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page
 
 #taxi = cudf.read_parquet("yellow_tripdata_2009-01.parquet")
 #read from files 01 to 12
+def haversine_distance(size, x1, y1, x2, y2, dist):
+    R = 6371.0  # Earth radius in kilometers
+    for i in range(size):
+        lat1 = np.radians(y1[i])
+        lon1 = np.radians(x1[i])
+        lat2 = np.radians(y2[i])
+        lon2 = np.radians(x2[i])
+
+        dlat = lat2 - lat1
+        dlon = lon2 - lon1
+
+        a = np.sin(dlat / 2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2)**2
+        c = 2 * np.arcsin(np.sqrt(a))
+
+        dist[i] = R * c
+
 query_expr = (
-    "Start_Lon >= -74.15 & Start_Lon <= -73.7004 & "
-    "Start_Lat >= 40.5774 & Start_Lat <= 40.9176 & "
-    "End_Lon >= -74.15 & End_Lon <= -73.7004 & "
-    "End_Lat >= 40.5774 & End_Lat <= 40.9176"
+    "Start_Lon >= -74.15 & "
+    "Start_Lat >= 40.5774 &"
+    "End_Lon <= -73.7004 & "
+    "End_Lat <= 40.9176"
 )
 start_lon = -74.15
 start_lat = 40.5774
 end_lon = -73.7004
 end_lat = 40.9176
+
 all_x1 = []
 all_y1 = []
 all_x2 = []
@@ -28,14 +44,14 @@ all_y2 = []
 
 for month in range(1, 13):
     month_str = f"{month:02d}"
-    file_path = f"/tmp/tlcdata/yellow_tripdata_2009-{month_str}.parquet"
+    file_path = f"Parquet_Data/yellow_tripdata_2009-{month_str}.parquet"
 
     print("Loading:", file_path)
     df = pd.read_parquet(file_path)
 
     # Filter
-    #df = df.query(query_expr)
-    df = df[(df['Start_Lon'] >= start_lon) & (df['Start_Lat'] >= start_lat) & (df['End_Lon'] <= end_lon) & (df['End_Lat'] <= end_lat)]
+    df = df.query(query_expr)
+    cur = cur[(cur['Start_Lon'] >= start_lon) & (cur['Start_Lat'] >= start_lat) & (cur['End_Lon'] <= end_lon) & (cur['End_Lat'] <= end_lat)]
     all_x1.append(df["Start_Lon"].to_numpy())
     all_y1.append(df["Start_Lat"].to_numpy())
     all_x2.append(df["End_Lon"].to_numpy())
@@ -52,7 +68,14 @@ del all_x1, all_y1, all_x2, all_y2
 size = len(x1)
 dist = np.zeros(size, dtype=np.float64)
 
-haversine_library.haversine_distance(size, x1, y1, x2, y2, dist)
+# Source - https://stackoverflow.com/q
+# Posted by gilbert8, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-11-20, License - CC BY-SA 4.0
 
+start = timeit.timeit()
+
+haversine_distance(size, x1, y1, x2, y2, dist)
+end = timeit.timeit()
+print(end - start)
 print("Kernel finished for", size, "rows.")
 print("Distances:", dist)
