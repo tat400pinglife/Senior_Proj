bbc2f4df4c683dc9b7caa4b5293fb7e4ce5599e3
tat400pinglife <dzhang765@gmail.com>
2025-11-25 13:09:43 -0500

    p3p2 code

--- MIdterm_code/p3p1.py
+++ MIdterm_code/p3p1.py
@@ -10,11 +10,12 @@ grouped = df.groupby('kind')
 print(grouped.max())
 
 # now with cuDF
-import cudf
-cdf = cudf.DataFrame({
-    'kind': ['cat', 'dog', 'cat', 'dog'],
-    'height': [9.1, 6.0, 9.5, 34.0],
-    'weight': [7.9, 7.5, 9.9, 198.0]
-})
-grouped_cudf = cdf.groupby('kind')
-print(grouped_cudf.max())
+# import cudf
+# cdf = cudf.DataFrame({
+#     'kind': ['cat', 'dog', 'cat', 'dog'],
+#     'height': [9.1, 6.0, 9.5, 34.0],
+#     'weight': [7.9, 7.5, 9.9, 198.0]
+# })
+# grouped_cudf = cdf.groupby('kind')
+# print(grouped_cudf.max())
+# okay it didn't work for me but the format should be correct
---
+++ MIdterm_code/p3p2.py
@@ -0,0 +1,15 @@
+import numpy as np
+#from sklearn.linear_model import LinearRegression
+from cuml.linear_model import LinearRegression
+
+
+X = np.array([3.3, 4.4, 5.5, 6.71, 6.93, 4.168, 9.779, 6.182, 7.59, 2.167, 7.042, 10.791, 5.313, 7.997, 5.654, 9.27, 3.1]).reshape(-1, 1)
+Y = np.array([1.7, 2.76, 2.09, 3.19, 1.694, 1.573, 3.366, 2.596, 2.53, 1.221, 2.827, 3.465, 1.65, 2.904, 2.42, 2.94, 1.3])
+
+model = LinearRegression()
+model.fit(X, Y)
+
+intercept = model.intercept_
+slope = model.coef_[0]
+
+print(f'Intercept: {intercept}, Slope: {slope}')
